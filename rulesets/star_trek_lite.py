81f0d7830e45c833ad8adbca39820587a3cbbc51
nkiu <nkiu@me.com>
2026-01-24 19:51:02 +0100

    Add Star Trek Lite ruleset with optimistic phased progress description

---
+++ rulesets/star_trek_lite.py
@@ -0,0 +1,6 @@
+from rulesets.harsh_realism import HarshRealismRuleset
+
+
+class StarTrekLiteRuleset(HarshRealismRuleset):
+    name = "star_trek_lite"
+    description = "Star Trek lite (optimistic, phased progress)"
